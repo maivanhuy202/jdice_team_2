ffb8d416e1a156ac0574a9564aaf688f501c03e4
HuyHoang <HuyHoang@HuyHoanggg>
2022-06-16 02:02:16 +0700

    Dòng 25 thiếu int 01:48 PM 16/06 _ Hoang Dòng 27 bỏ "\\" 01:48 PM 16/06 _ Hoang Dòng 53 thiếu ";" 01:48 PM 16/06 _ Hoang Dòng 32 thiếu khai báo kiểu dữ liệu void 01:50 PM 16/06 _ Hoang Dòng 42 sửa rndnextInt -> rnd.nextInt(int) 01:55 PM 16/06 _ Hoang

--- DieRoll.java
+++ DieRoll.java
@@ -22,14 +22,14 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 
 public class DieRoll {
-    private ndice;
+    private int ndice;
     private int nsides;
-//    private int bonus;
+    private int bonus;
     private static Random rnd;
     static{
 	rnd=new Random();
     }
-    public Die_Roll(int ndice,
+    public void Die_Roll(int ndice,
 		   int nsides,
 		   int bonus) {
 	this.ndice=ndice;
@@ -39,18 +39,19 @@ public class DieRoll {
     public RollResult makeRoll() {
 	RollResult r=new RollResult(bonus);
 	for(int i=0;i<ndice;i++) {
-	    int roll=rndnextInt(nsides)+1;
+	    int roll=rnd.nextInt(nsides)+1;
 	    r.addResult(roll);
 	}
 	return r;
     }
-    public String toString() {
+
+	public String toString() {
 	String ans =ndice+"d"+nsides;
 	if(bonus>0) {
 	    ans= ans+"+"+bonus;
 	}
 	else if(bonus<0) {
-	    ans=ans+bonus
+	    ans=ans+bonus;
 	}
 	return ans;
     }
