3767c7c4f8d746cc23009f0609f552727bd09586
HuyHoang <HuyHoang@HuyHoanggg>
2022-06-16 02:15:53 +0700

    format code 02:14 16/06 AM Do Huy Hoang sửa dòng 30 void Die_Roll -> DieRoll  02:14 16/06 AM Do Huy Hoang

--- DieRoll.java
+++ DieRoll.java
@@ -20,40 +20,36 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
  */
 
-
 public class DieRoll {
-    private int ndice;
-    private int nsides;
-    private int bonus;
-    private static Random rnd;
-    static{
-	rnd=new Random();
-    }
-    public void Die_Roll(int ndice,
-		   int nsides,
-		   int bonus) {
-	this.ndice=ndice;
-	this.nsides=nsides;
-	this.bonus=bonus;
-    }
-    public RollResult makeRoll() {
-	RollResult r=new RollResult(bonus);
-	for(int i=0;i<ndice;i++) {
-	    int roll=rnd.nextInt(nsides)+1;
-	    r.addResult(roll);
+	private int ndice, nsides, bonus;
+	private static Random rnd;
+	static {
+		rnd = new Random();
 	}
-	return r;
-    }
+
+	public DieRoll(int ndice, int nsides, int bonus) {
+		this.ndice = ndice;
+		this.nsides = nsides;
+		this.bonus = bonus;
+	}
+
+	public RollResult makeRoll() {
+		RollResult r = new RollResult(bonus);
+		for (int i = 0; i < ndice; i++) {
+			int roll = rnd.nextInt(nsides) + 1;
+			r.addResult(roll);
+		}
+		return r;
+	}
 
 	public String toString() {
-	String ans =ndice+"d"+nsides;
-	if(bonus>0) {
-	    ans= ans+"+"+bonus;
+		String ans = ndice + "d" + nsides;
+		if (bonus > 0) {
+			ans = ans + "+" + bonus;
+		} else if (bonus < 0) {
+			ans = ans + bonus;
+		}
+		return ans;
 	}
-	else if(bonus<0) {
-	    ans=ans+bonus;
-	}
-	return ans;
-    }
 
 }
